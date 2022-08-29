78f32a0f5002dbfd6dc09da7d386caf1a4c2a594
Pedro <pedro.silva.dev.online@gmail.com>
2022-08-29 17:53:26 -0300

    primeiro commit

---
+++ src/main/java/br/com/solipy/ariestreineapi/AriesTreineApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.solipy.ariestreineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriesTreineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AriesTreineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/solipy/ariestreineapi/AriesTreineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.solipy.ariestreineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriesTreineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
